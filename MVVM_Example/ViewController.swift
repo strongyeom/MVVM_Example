52a07b9ad90eae6cb8469d0d9eb24a961d4cd97e
yeomsungpil <present7256@gmail.com>
2023-09-12 16:09:29 +0900

    Initial Commit

---
+++ MVVM_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Example
+//
+//  Created by 염성필 on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
